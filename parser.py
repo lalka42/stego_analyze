ec6d6e87965278d7e4900e68fad7d1cbef13538f
Nikita <paskidov@internet.ru>
2022-09-06 00:20:56 +0300

    test new parser algorithm

--- parser.py
+++ parser.py
@@ -1,7 +1,14 @@
 from scapy.all import *
-
+import pandas as pd
 pth = 'D:\Desktop\IP.pcap'
 pkts = rdpcap(pth)
 
-print(ls(pkts[7]))
-#print(pkts[7].seq)
+frame = pd.DataFrame({'udp.srcport': [],'udp.dstport':[],'ip.src':[],'ip.dst':[]})
+udpsprt = pd.Series(range(9))
+for i in range(9):
+    udpsprt[i] = pkts[i].sport
+    #frame.loc[i] = pkts[i].sport
+    #frame.loc[i] = pkts[i].dport
+    #print(pkts[i].sport)
+#frame.to_excel('test.xlsx')
+print(udpsprt)
