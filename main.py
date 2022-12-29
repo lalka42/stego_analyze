7f190c68ad70d78aa56caaa78593a762e8d2131d
Nikita <hrukker0@gmail.com>
2022-12-29 14:57:29 +0300

    Доделан интерфейс для анализа дампов

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter
 from tkinter.ttk import Combobox  
 from tkinter.ttk import Checkbutton
 import tkinter.filedialog as fd
@@ -7,54 +8,63 @@ from new_algs import knn
 import os
 import time
 import subprocess
+from tkinter import messagebox
+import sys
 
 
 dumpe_file = os.getcwd() + '\\dump.csv'
 #Функция завершения работы программы
 def quit():
+    variable.quit = 1
     dump_file = os.getcwd() + '\\dump.csv'
     if os.path.exists(dump_file):
         os.remove(dump_file)
     window.destroy()
 
+
+
 #Получение пути дампа
 def dumped():
-    filename = fd.askopenfilename(filetypes=(
-        ("pcapng", "*.pcapng"), ("all files", "*.*")))
+    filename = fd.askopenfilename(filetypes=(("pcapng", "*.pcapng"), ("all files", "*.*")))
     variable.change_path(filename)
+    file_name = os.path.basename("r'" + variable.path)
+    Label(window, text=file_name, bg='#7FFFD4', font=('arial', 10, 'normal')).place(x=30, y=180)
 
-#Кнопка Анализ
+def saved():
+    save_dir_path = fd.askdirectory()
+    variable.change_save_path(save_dir_path)
+    res_save_path = variable.path_of_save
+    Label(window, text=res_save_path, bg='#7FFFD4', font=('arial', 10, 'normal')).place(x=30, y=310)
+
+#Кнопка ПУСК
 def analyze():
-    wd = os.getcwd()
-    wd = '"' + wd + '\\Wireshark' + '\\tshark.exe' + '"'
-    wd = wd + ' -r ' + variable.path + " -T fields -E header=y -E separator=, -E occurrence=f -e udp.srcport -e udp.dstport -e tcp.srcport -e tcp.dstport -e tcp.ack -e tcp.urgent_pointer -e tcp.window_size_value -e ip.len -e ip.id -e ip.proto -e ip.dsfield.dscp -e ip.dsfield.ecn -e ip.src -e ip.dst > "  + '"' + dumpe_file + '"'
-    subprocess.call(wd, shell=True)
-    if c1 == 1:
-        knn(dumpe_file)
-
-    
-
-   
+    if variable.path == None:
+        messagebox.showerror("Ошибка", "Не выбран дамп для анализа")
+        return None
+    elif variable.path_of_save == None:
+        messagebox.showerror("Ошибка", "Не выбрана директория для сохранения результатов")
+        return None
+    else:
+     wd = os.getcwd()
+     wd = '"' + wd + '\\Wireshark' + '\\tshark.exe' + '"'
+     wd = wd + ' -r ' + variable.path + " -T fields -E header=y -E separator=, -E occurrence=f -e udp.srcport -e udp.dstport -e tcp.srcport -e tcp.dstport -e tcp.ack -e tcp.urgent_pointer -e tcp.window_size_value -e ip.len -e ip.id -e ip.proto -e ip.dsfield.dscp -e ip.dsfield.ecn -e ip.src -e ip.dst > "  + '"' + dumpe_file + '"'
+     subprocess.call(wd, shell=True)
+     knn(dumpe_file)
 
 #Отрисовка окна
 window = Tk()
-window.title("Machine learning")
-
-lbl = Label(window, text=" ")  
-lbl.grid(column=0, row=0)  
+window.title("Стегоанализ")
 
 #Кнопка выбора дампа
-Button(window, text="Выбрать дамп", bg = "#ffa500", font=('Arial Bold', 16), fg = 'white', command=dumped).place(x=200,y=50, anchor=CENTER)
+Button(window, text="Выбрать дамп", bg = "#ffa500", font=('Arial Bold', 14), fg = 'white', command=dumped).place(x=116,y=230, anchor=CENTER)
 
-
-lbl = Label(window, text=" ")  
-lbl.grid(column=0, row=2)
+#Кнопка выбора куда сохранять диаграмму и результаты
+Button(window, text="Выбрать директорию", bg = "#ffa500", font=('Arial Bold', 12), fg = 'white', command=saved).place(x=116,y=360, anchor=CENTER)
 
 #Кнопка закрытия программы
 Button(window, text="Закрыть программу", command=quit).place(x=425,y=500, anchor=CENTER)
 
-
-#Выбор протокола
+#Выбор режима работы программы
 program_mode=1
 
 frame=Frame(window, width=0, height=0, bg='#7FFFD4')
@@ -74,8 +84,19 @@ Button(window, text="ПУСК", bg = "#ffa500", font=('GOST', 16),fg = 'white', 
 Label(window, text='Пожалуйста, выберите режим работы программы', bg='#7FFFD4', font=('arial', 12, 'normal')).place(x=270, y=65)
 Label(window, text='Обучение', bg='#7FFFD4', font=('arial', 16, 'normal')).place(x=621, y=145)
 Label(window, text='Анализ', bg='#7FFFD4', font=('arial', 16, 'normal')).place(x=82, y=145)
-
+Label(window, text='(Куда сохранять результаты)', bg='#7FFFD4', font=('arial', 10, 'normal')).place(x=27, y=380)
 window.geometry('850x530')
 window.configure(background='#7FFFD4')
+
+Check_diag = IntVar()
+Checkbutton(window, text='Нужно ли вывести диаграмму?', variable=Check_diag).place(x=40, y=260)
+
+
+Check_savediag = IntVar()
+Checkbutton(window, text='Нужно ли сохранить диаграмму?', variable=Check_savediag).place(x=40, y=280)
+
+file_name = IntVar()
+
+
 window.mainloop()
 
--- variable.py
+++ variable.py
@@ -4,11 +4,16 @@ class Variable():
     def __init__(self, **kwargs):
         self.path = kwargs.get('path')
         self.combo = kwargs.get('combo')
+        self.path_of_save = kwargs.get('path_2')
+        self.quit = 0
 
     def change_path(self, path):
         self.path = path
         '''if self.path is not None:
             self.path = '"' + self.path + '"'''
+
+    def change_save_path(self, path_2):
+        self.path_of_save = path_2
     
     def change_combo(self, combo):
         self.combo = combo
